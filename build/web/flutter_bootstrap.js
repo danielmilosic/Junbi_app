9d4f7d2fac2f579ab38ffe831b2328da4e92412b
danielmilosic <daniel.milosic@edu.uni-graz.at>
2025-10-15 13:08:27 +0200

    added assets

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1394368540"
+    serviceWorkerVersion: "170999197"
   }
 });
